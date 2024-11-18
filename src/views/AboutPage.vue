ccf19c8e0db5f2eda77fc951ee3f5f1ca41eb142
boukthir.hafedh@gmail.com <Haddoula@2024>
2024-11-18 11:11:10 +0100

    Initial commit

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>À Propos</h1>
+    <p>Ceci est la page à propos.</p>
+  </div>
+</template>
---
+++ src/views/UserPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Utilisateur</h1>
+    <p>Informations sur l'utilisateur.</p>
+  </div>
+</template>
